5169d790fea23b3f0ec403757d3799be4542c51d
Benjtalkshow <chinedubenj@gmail.com>
2023-09-26 05:28:39 -0700

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,87 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://i.ibb.co/vwF4bNp/stayInn.png",
+    title: "StayInn Booking",
+    description: "A hotel booking platform, where your lodging experience becomes easier and more enjoyable.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://github.com/Benjtalkshow/stay-inn-estate-platform",
+    demo_url: "https://stay-inn-hotel.netlify.app/",
+  },
+  {
+    img: "https://i.ibb.co/zJZrN2g/airline-Dashboard.png",
+    title: "Upward Airline Dashboard",
+    description:
+      "Upward Airlines Corporate Dashboard: Web-based app for monitoring airline performance, with intuitive interface, grid layout, and key features for data analysis and decision-making by administrators and employees.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://github.com/Benjtalkshow/Upward-Airlines-Corporate-Dashboard",
+    demo_url: "https://upward-airlines-corporate-dashboard.netlify.app/",
+  },
+  {
+    img: "https://i.ibb.co/X7FzSxt/movieBox.png",
+    title: "MovieBox",
+    description: "The TMDB Movie App is an interactive movie application that retrieves movie data from The Movie Database (TMDb) API.",
+    tech_stack: "Reactjs, Tailwindcss",
+    github_url: "https://github.com/Benjtalkshow/HNG-TaskTwo",
+    demo_url: "https://hng10-task-two.netlify.app/",
+  },
+  {
+    img: "https://i.ibb.co/vdZD3Ld/quizApk.png",
+    title: "Quiz Web App",
+    description:
+      "Ultimate Online Quiz Project for newbies, in FrontEnd development.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://github.com/Benjtalkshow/quizapp",
+    demo_url: "https://quiz-apk.netlify.app/",
+  },
+  {
+    img: "https://i.ibb.co/vmbMtwf/gallery.png",
+    title: "CkaePixie Gallery",
+    description:
+      "The TMDB Movie App is an interactive movie application that retrieves movie data from The Movie Database (TMDb) API.",
+    tech_stack: "Reactjs, Tailwindcss",
+    github_url: "https://github.com/Benjtalkshow/HNG-TaskTwo",
+    demo_url: "https://hng10-task-two.netlify.app/"
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png",
+    name: "HTML",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png",
+    name: "CSS",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
+    name: "JavaScript",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+    name: "ReactJS",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/202896760-337261ed-ee92-4979-84c4-d4b829c7355d.png",
+    name: "Tailwind",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/189716855-2c69ca7a-5149-4647-936d-780610911353.png",
+    name: "Firebase",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive Design",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/25181517/192158957-b1256181-356c-46a3-beb9-487af08a6266.png",
+    name: "Wordpress",
+  },
+];
